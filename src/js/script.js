cf89920d3c7e9c6b58377bdfc08d8aa73afc3d8a
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2024-12-04 10:51:33 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 // *** LIGHTBOX ***
+const lightBox = document.querySelector("#lightbox");
+
+
+const image = document.querySelector("#lightbox img");
+
+
+document.addEventListener("click", function (e) {
+  
+  if (e.target.matches("[data-full-img]") || lightBox.open) {
+    
+    if (lightBox.open) {
+      lightBox.close();
+    } else {
+      
+      lightBox.showModal();
+    }
+
+    image.src = e.target.dataset.fullImg;
+  }
+});
